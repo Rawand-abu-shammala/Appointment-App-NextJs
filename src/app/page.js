ee642a2d41178f6a42402bd764d977fec75c46dd
Rawand-abu-shammala <rwndsami@gmail.com>
2026-02-12 15:53:52 +0200

    feat(auth): add Header and Hero components with Kinde integration

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
-import { Button } from "@/components/ui/button"
+import Header from "./_components/Header";
+import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div>
-      <Button>Click me</Button>
+      <Header/>
+      <Hero/>
     </div>
   );
 }
